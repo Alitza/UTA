1f27e8b33b12b8f416bd590e5a5e4869e62deb07
Alitza <alitza-konata.13@hotmail.com>
2017-05-03 12:13:44 -0500

    Cambios para la segunda pantalla y tercera

---
+++ app/src/main/java/uta/uta/Home.java
@@ -0,0 +1,8 @@
+package uta.uta;
+
+/**
+ * Created by Dora Mar on 3/05/17.
+ */
+
+public class Home {
+}
--- app/src/main/java/uta/uta/MainActivity.java
+++ app/src/main/java/uta/uta/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final  R = ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
